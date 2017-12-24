f54747575aec647a48e71b422f50d99031928ad1
Paul LaRue <laruep@rpi.edu>
2017-12-24 13:27:54 -0500

    Changed orientation of camera and location of sun to mimic 2D

--- core.js
+++ core.js
@@ -17,7 +17,7 @@
 //Math contants and such
 var pi = Math.PI;
 
-
+// 
 var camera, scene, renderer;
 var geometry, material, player;
 
@@ -34,36 +34,37 @@ var BLUE = new THREE.Color( 0x0000ff );
 
 var cubeColors = [ RED, GREEN, BLUE ];
 
-init();
+init(); 
 
 function init() {
-    // Createss a camera object
+// Createss a camera object
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    // This wil change the camera to be slightly above the normal plane to make scene appear really 3D, may not need this
-    camera.position.set(1,1,1);
-    // Creates a scene object
+// This wil change the camera to be slightly above the normal plane to make scene appear really 3D, may not need this
+    camera.position.set( 0 , 0 , 10);
+// Creates a scene object
     scene = new THREE.Scene();
 
-    // Create renderer and set its size
+// Create renderer and set its size
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor(new THREE.Color(0x111111));
 
-    // Create sphere mesh from geometry and Lambert (light-reactive) material
+// Create sphere mesh from geometry and Lambert (light-reactive) material
     geometry = new THREE.SphereGeometry( .5 , 20, 20);
-    material = new THREE.MeshLambertMaterial( { color: 0xff0000 , } );
+    material = new THREE.MeshBasicMaterial( { color: 0xdddd00 } );
     centerPlanet = new THREE.Mesh( geometry, material );
 
-    // Create point representing sun
+// Create point representing sun
     var Sun = new THREE.PointLight( 0xffffff , 1 , 0 );
-    Sun.position.set( 0 , 10 , 0 );
+    Sun.position.set( 0 , 0 , 0 );
     scene.add( Sun );
 
 
-    // Adds the center planet to the scene and center camera on it
+// Adds the center planet to the scene and center camera on it
     scene.add( centerPlanet );
     camera.lookAt(centerPlanet.position);
 
+// Tells the script where to output the WebGL output and then renders the output
     document.getElementById("output").appendChild(renderer.domElement);
     renderer.render(scene, camera);
-
 }
